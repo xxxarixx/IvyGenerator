a65e4e531e1d2589251bdce573dbe23bf3d0370c
IndieMoose <69750932+xxxarixx@users.noreply.github.com>
2024-09-15 16:15:33 +0200

    Full visualization support

--- NewGreatProject/Assets/Scripts/GridVisualization.cs
+++ NewGreatProject/Assets/Scripts/GridVisualization.cs
@@ -1,10 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 using UnityEngine;
-namespace Grid
+using static HorizontalGraph.HorizontalGrid;
+namespace HorizontalGraph
 {
+    [RequireComponent(typeof(HorizontalGrid))]
     public class GridVisualization : MonoBehaviour
     {
-        
+        private HorizontalGrid grid;
+        public Color pointColor = Color.white;
+        public Color linesColor = Color.gray;
+        public Color gridColor = Color.gray;
+        public Color viewPortColor = Color.green;
+        [Range(0.01f,1f)]
+        public float gizSize;
+        private List<Vector3> vis = new List<Vector3>();
+        public int maxRecordsToShow;
+        [Range(0,100)]
+        public int timeOffset;
+        private List<Vector2> lastPoints = new List<Vector2>();
+        public void Visualize(List<Vector2> points)
+        {
+            //if(lastPoints.Count != points.Count)
+            vis = grid.GenerateGraph(points.ToArray(), timeOffset,maxRecordsToShow);
+            lastPoints = points;
+        }
+        public void VisualizeLastVisualize()
+        {
+            Visualize(lastPoints);
+        }
+        private void OnValidate()
+        {
+            grid = GetComponent<HorizontalGrid>();
+            VisualizeLastVisualize();
+        }
+        private void OnDrawGizmos()
+        {
+            if (grid == null)
+                OnValidate();
+            DrawGraphViewPort();
+            if (vis.Count == 0 || lastPoints.Count == 0)
+                return;
+            DrawPoints();
+        }
+        private void DrawPoints()
+        {
+            for (int i = -1; i < vis.Count; i++)
+            {
+                if(i != -1)
+                {
+                    Vector3 worldPosition = vis[i];
+                    Gizmos.color = pointColor;
+                    Gizmos.DrawSphere(worldPosition, gizSize);
+                }
+
+                Gizmos.color = pointColor;
+                Vector3 startPos = i == -1 ? grid.startingPosition.position : vis[i];
+                Vector3 endPos = i >= vis.Count - 1 ? vis[i] : vis[i + 1];
+                Gizmos.DrawLine(startPos, endPos);
+            }
+        }
+        private void DrawGraphViewPort()
+        {
+            float vpWidth = maxRecordsToShow * grid.gridSpacing;
+            float vpHeight = grid.maxHeight;
+            Vector3 viewPortCenter = grid.startingPosition.position + (Vector3)grid.offset + new Vector3(vpWidth / 2, vpHeight / 2);
+            float gridWidth = vpWidth / maxRecordsToShow;
+            //grid
+            Gizmos.color = gridColor;
+            for (int i = 0; i < Mathf.Clamp(vis.Count, 0, maxRecordsToShow); i++)
+            {
+                Vector3 worldPosition = vis[i];
+                Gizmos.DrawWireCube(new Vector3(worldPosition.x + gridWidth / 2, viewPortCenter.y), new Vector3(gridWidth, vpHeight));
+            }
+            //viewPort
+            Gizmos.color = viewPortColor;
+            Gizmos.DrawWireCube(viewPortCenter, new Vector3(vpWidth,vpHeight));
+
+        }
     }
 }
--- NewGreatProject/Assets/Scripts/HorizontalGrid.cs
+++ NewGreatProject/Assets/Scripts/HorizontalGrid.cs
@@ -1,23 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-namespace Grid
+namespace HorizontalGraph
 {
+    [DefaultExecutionOrder(-1), ExecuteAlways]
     public class HorizontalGrid : MonoBehaviour
     {
-        public static HorizontalGrid instance { get; private set; }
 
         /// <summary>
         /// key => time; value => node on grid
         /// </summary>
-        public Dictionary<float,Node> grid = new Dictionary<float,Node>();
+        public Dictionary<int,Node> grid = new Dictionary<int, Node>();
         public Transform startingPosition;
-        [Range(1,100)]
-        public int timeStamp = 20;
+        public Vector2 offset = new Vector2(1.5f, 1.5f);
+        public float gridSpacing;
+        public float maxHeight = 10f;
+        public GridVisualization gridVisualization;
         [System.Serializable]
         public class Node
         {
-            public float value;
+            public readonly float value;
+            public readonly Vector3 worldPosition;
+            public Node(int time, float value, HorizontalGrid grid)
+            {
+                this.value = value;
+                this.worldPosition = GridToWorld(time, value, grid);
+            }
+            public Vector3 GridToWorld(int time, float value, HorizontalGrid grid)
+            {
+                value = grid.maxHeight / value;
+                return grid.startingPosition.position + (Vector3)grid.offset + new Vector3(time * grid.gridSpacing,value);
+            }
+        }
+        public List<Vector3> GenerateGraph(Vector2[] graphPoints, int timeOffset, int maxRecords = int.MaxValue)
+        {
+            if (startingPosition == null)
+                return default;
+            grid.Clear();
+            graphPoints.OrderBy(p => p.x);
+            maxRecords = Mathf.Clamp(maxRecords, 0, graphPoints.Length - 1);
+            timeOffset = Mathf.Clamp(timeOffset, 0, graphPoints.Length - 2);
+            for (int i = timeOffset; i <= Mathf.Clamp(graphPoints.Length,1,maxRecords); i++)
+            {
+                Vector2 point = graphPoints[i];
+                point.x = point.x - timeOffset;
+                if (grid.ContainsKey((int)point.x))
+                    continue;
+                grid.Add((int)point.x,new Node((int)point.x,point.y,this));
+            }
+            return grid.Select(p => p.Value.worldPosition).ToList();
+        }
+        private void OnValidate()
+        {
+            if (gridVisualization == null)
+                return;
+            gridVisualization.VisualizeLastVisualize();
         }
     }
 }
