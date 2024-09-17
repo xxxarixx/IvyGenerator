ed6cc83824ee5134a172c7be6e80c0da6e8fce5c
IndieMoose <69750932+xxxarixx@users.noreply.github.com>
2024-09-17 14:02:20 +0200

    Support for time offset in records

--- NewGreatProject/Assets/Scripts/HorizontalGrid.cs
+++ NewGreatProject/Assets/Scripts/HorizontalGrid.cs
@@ -37,16 +37,23 @@ namespace HorizontalGraph
         {
             if (startingPosition == null)
                 return default;
+            ///Setup Veriables
             grid.Clear();
-            graphPoints.OrderBy(p => p.x);
-            maxRecords = Mathf.Clamp(maxRecords, 0, graphPoints.Length - 1);
-            timeOffset = Mathf.Clamp(timeOffset, 0, graphPoints.Length - 2);
-            for (int i = timeOffset; i <= Mathf.Clamp(graphPoints.Length,1,maxRecords); i++)
+            graphPoints = graphPoints.OrderBy(p => p.x).ToArray();
+            int maxRange = graphPoints.Length - 1;
+            maxRecords = Mathf.Clamp(maxRecords, 0, maxRange);
+            timeOffset = Mathf.Clamp(timeOffset, 0, maxRange - 1);
+            ///Get selected record range
+            int from = timeOffset;
+            int to = Mathf.Clamp(Mathf.Clamp(graphPoints.Length, 1, maxRecords) + timeOffset,0, maxRange);
+            for (int i = from; i <= to; i++)
             {
                 Vector2 point = graphPoints[i];
+                ///Make sure that even with applied timeOffset first point will start from first position
                 point.x = point.x - timeOffset;
                 if (grid.ContainsKey((int)point.x))
                     continue;
+                ///Key => Time (position X), Value => Node
                 grid.Add((int)point.x,new Node((int)point.x,point.y,this));
             }
             return grid.Select(p => p.Value.worldPosition).ToList();
